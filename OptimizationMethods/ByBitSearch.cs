8330aff2034a4816d1840d85f297273f6fc22cd4
Даниил Поминов <d.pominov2016@yandex.ru>
2024-10-15 14:16:17 +0700

    Добавьте файлы проекта.

---
+++ OptimizationMethods/ByBitSearch.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptimizationMethods
+{
+    public class ByBitSearch
+    {
+            public static double SearchByBit(Func<double, double> function, double a, double b, double epsilon)
+            {
+
+                double x_0, x_1, f_0, f_1, h;
+                goto first;
+            first:
+                {
+                    h = (b - a) / 4;
+                    x_0 = a;
+                    f_0 = function(x_0);
+                    goto second;
+                }
+            second:
+                {
+                    x_1 = x_0 + h;
+                    f_1 = function(x_1);
+                    goto thirth;
+                }
+            thirth:
+                {
+                    if (f_0 > f_1)
+                    {
+                        goto fourth;
+                    }
+                    else
+                    {
+                        goto fifth;
+                    }
+
+                }
+            fourth:
+                {
+                    x_0 = x_1;
+                    f_0 = f_1;
+                    if (a < x_0 && x_0 < b)
+                    {
+                        goto second;
+                    }
+                    else
+                    {
+                        goto fifth;
+                    }
+                }
+            fifth:
+                {
+                    if (Math.Abs(h) <= epsilon)
+                    {
+                        return x_0;
+                    }
+                    else
+                    {
+                        goto sixth;
+                    }
+                sixth:
+                    {
+                        x_0 = x_1;
+                        f_0 = f_1;
+                        h = -h / 4.0;
+                        goto second;
+                    }
+                }
+            }
+        }
+    }
---
+++ OptimizationMethods/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace OptimizationMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 2.3;
+            double b = 3.01;
+            //double a = 1;
+            //double b = 2;
+            double epsilon = Math.Pow(10, -6);
+
+            Func<double, double> function = (x) => -Math.Cbrt((4 - x) * 2 * (Math.Pow(x - 1, 2)));
+            //Func<double, double> function = (x) => 2*x+1/x;
+
+            var result = ByBitSearch.SearchByBit(function, a, b, epsilon);
+            var val = function(result);
+
+
+            Console.WriteLine($"Минимум на отрезке [{a};{b}]: \nx* = {result}, f(x*) = {val}");
+        }
+    }
+}
