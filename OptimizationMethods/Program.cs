07f253d86392b7fa54cd12f96f7af6a912ce8214
Daniil Pominov <d.pominov2016@yandex.ru>
2025-03-02 21:47:33 +0700

    first version of alghoritm

--- OptimizationMethods/Program.cs
+++ OptimizationMethods/Program.cs
@@ -1,6 +1,8 @@
-﻿using MathNet.Symbolics;
+﻿using MathNet.Numerics.LinearAlgebra;
+using MathNet.Symbolics;
 using OptimizationMethods.Approximations;
 using OptimizationMethods.GradientDescent;
+using OptimizationMethods.SecondOrderMethods;
 using Expr = MathNet.Symbolics.SymbolicExpression;
 
 namespace OptimizationMethods
@@ -11,6 +13,7 @@ namespace OptimizationMethods
         {
 
             double epsilon = Math.Pow(10, -6);
+            double tolerance = 0.1;
 
             var x = Expr.Variable("x");
             var y = Expr.Variable("y");
@@ -21,9 +24,22 @@ namespace OptimizationMethods
             var x1 = 1.0;
             var x2 = 2.0;
             var x3 = 0.0;
-            Expr func = x.Pow(5) -5*x + +10*(-x.Pow(2)+y).Pow(2)+z.Pow(3)-1-6*z;
-            var result = StepSplitting.Search(func, x, y, z, (x1, x2, x3), epsilon, 0.1,null);
-            Console.WriteLine(result);
+            var testPoints = new[] { 
+                Vector<double>.Build.Dense([x1, x2, x3]),
+                Vector<double>.Build.Dense([-1.0, 10.0, 1.0]),
+                Vector<double>.Build.Dense([-x1, -x2, x3])
+            };
+            //Expr func = x.Pow(5) -5*x + +10*(-x.Pow(2)+y).Pow(2)+z.Pow(3)-1-6*z;
+            Expr func = 2*x.Pow(4)*y.Pow(4)+x.Pow(2)*y.Pow(2)+z.Pow(4)+x.Pow(2)*z.Pow(2)+x+y;
+            //var result = StepSplitting.Search(func, x, y, z, (x1, x2, x3), epsilon, 0.1,null);
+            foreach (var point in testPoints)
+            {
+                Console.WriteLine(point);
+                var result = Markvardt.Search(func, x, y, z, point, epsilon, tolerance, 10000);
+                Console.WriteLine(result);
+            }
+            
+            
         }
     }
 }
--- OptimizationMethods/SecondOrderMethods/Markvardt.cs
+++ OptimizationMethods/SecondOrderMethods/Markvardt.cs
@@ -1,12 +1,74 @@
-using System;
-using Microsoft.FSharp.Data.UnitSystems.SI.UnitNames;
 using MathNet.Symbolics;
-using OptimizationMethods;
+using MathNet.Numerics.LinearAlgebra;
+using Expr = MathNet.Symbolics.SymbolicExpression;
+using MathNet.Numerics.Optimization;
 
 namespace OptimizationMethods.SecondOrderMethods;
 
 public class Markvardt : IMethod
 {
+    public static Vector<double> Search(Expr f, Expr x, Expr y, Expr z, Vector<double> initialGuess, double epsilon, double tolerance, int maxIterations)
+    {
+        //градиент функции
+        var gradient = new[] { f.Differentiate(x), f.Differentiate(y), f.Differentiate(z) };
+
+        //гессиан функции
+        var hessian = new[,]
+        {
+            { f.Differentiate(x).Differentiate(x), f.Differentiate(x).Differentiate(y), f.Differentiate(x).Differentiate(z) },
+            { f.Differentiate(y).Differentiate(x), f.Differentiate(y).Differentiate(y), f.Differentiate(y).Differentiate(z) },
+            { f.Differentiate(z).Differentiate(x), f.Differentiate(z).Differentiate(y), f.Differentiate(z).Differentiate(z) }
+        };
+        var currentPoint = initialGuess;
+        var grad = EvaluateGradient(gradient, currentPoint);
+        var hess = EvaluateHessian(hessian, currentPoint);
+        int k = 0;
+        checkEnd:
+        {
+            grad = EvaluateGradient(gradient, currentPoint);
+            if (grad.L2Norm() <= epsilon || k>maxIterations) {
+#if DEBUG
+                Console.WriteLine($"Алгоритм остановился после {k} итераций");
+#endif
+                return currentPoint;
+            }
+            //goto evalute;
+            hess = EvaluateHessian(hessian, currentPoint);
+        }
+        evalute:
+        {
+            //var hess  = EvaluateHessian(hessian,currentPoint);
+            
+            //единичная матрица
+            var identityMatrix = Matrix<double>.Build.DenseIdentity(3);
+
+            var nextPoint = currentPoint - (hess + tolerance * identityMatrix).Inverse()*grad;
+
+            if (f.Evaluate(new Dictionary<string, FloatingPoint> { { "x", nextPoint[0] }, { "y", nextPoint[1] }, { "z", nextPoint[2] } }).RealValue <
+                f.Evaluate(new Dictionary<string, FloatingPoint> { { "x", currentPoint[0] }, { "y", currentPoint[1] }, { "z", currentPoint[2] } }).RealValue)
+            {
+                currentPoint = nextPoint;
+                k += 1;
+                tolerance /= 2;
+                if (tolerance <= 1.0 / 10000000.0)
+                {
+                    Console.WriteLine(currentPoint);
+                    throw new MaximumIterationsException("tolerance become too small");
+                }
+                    goto checkEnd;
+            }
+            else
+            {
+                tolerance *= 2;
+                if(tolerance>= 10000000)
+                {
+                    Console.WriteLine(currentPoint);
+                    throw new MaximumIterationsException("tolerance become too big");
+                }
+                goto evalute;
+            }
+        }
+    }
     public static double Search(SymbolicExpression function,double x0, double epsilon, SymbolicExpression X)
     {
         var GetPointX = Common.GetPointX;
@@ -54,6 +116,38 @@ public class Markvardt : IMethod
                 goto second;
             }
         }
+    }
+    static Vector<double> EvaluateGradient(Expr[] gradient, Vector<double> point)
+    {
+        var result = Vector<double>.Build.Dense(3);
+        var symbols = new Dictionary<string, FloatingPoint>
+        {
+            { "x", point[0] },
+            { "y", point[1] },
+            { "z", point[2] }
+        };
+
+        for (int i = 0; i < 3; i++)
+            result[i] = gradient[i].Evaluate(symbols).RealValue;
+
+        return result;
+    }
+
+    static Matrix<double> EvaluateHessian(Expr[,] hessian, Vector<double> point)
+    {
+        var result = Matrix<double>.Build.Dense(3, 3);
+        var symbols = new Dictionary<string, FloatingPoint>
+        {
+            { "x", point[0] },
+            { "y", point[1] },
+            { "z", point[2] }
+        };
+
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 3; j++)
+                result[i, j] = hessian[i, j].Evaluate(symbols).RealValue;
+
+        return result;
     }
 
 }
