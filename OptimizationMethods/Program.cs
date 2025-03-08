3f224b96c4efe3ba623f11f7388a642b71683ced
Daniil Pominov <d.pominov2016@yandex.ru>
2025-03-08 22:46:40 +0700

    Переписал под любую размерность, добавил проверку положительной определенности Гёссиана

--- OptimizationMethods/Program.cs
+++ OptimizationMethods/Program.cs
@@ -12,29 +12,31 @@ namespace OptimizationMethods
         static void Main(string[] args)
         {
 
-            double epsilon = Math.Pow(10, -6);
-            double tolerance = 10000;
+            double epsilon = Math.Pow(10, -1);
+            double tolerance = 20;
 
             var x = Expr.Variable("x");
             var y = Expr.Variable("y");
             var z = Expr.Variable("z");
-            var x1 = -1.0;
+            var x1 = 0.5;
             var x2 = 1.0;
-            var x3 = -1.0;
+            //var x3 = 0.0;
             //var x1 = -0.5;
             //var x2 = -0.5;
             //var x3 = 0.0;
             var testPoints = new[] { 
-                Vector<double>.Build.Dense([x1, x2, x3]),
+                Vector<double>.Build.Dense([x1, x2]),
             };
             //Expr func = x.Pow(5) -5*x + +10*(-x.Pow(2)+y).Pow(2)+z.Pow(3)-1-6*z;
-            Expr func = 2*x.Pow(4)*y.Pow(4)+x.Pow(2)*y.Pow(2)+z.Pow(4)+x.Pow(2)*z.Pow(2)+x+y;
+            //Expr func = 2*x.Pow(4)*y.Pow(4)+x.Pow(2)*y.Pow(2)+z.Pow(4)+x.Pow(2)*z.Pow(2)+x+y;
             // for initial (-1/2,-1/2,0) Critical point found at (x, y, z) = (-0.658417, -0.658417, 0.000000)
+            Expr func = 2*x.Pow(2)+x*y+y.Pow(2);
+            //Example from Panteleev's book, works with (0.5,1), E = 0.1, tol = 20;
             Console.WriteLine(func);
             foreach (var point in testPoints)
             {
                 Console.WriteLine(point);
-                var result = Markvardt.Search(func, x, y, z, point, epsilon, tolerance, 10000);
+                var result = Markvardt.Search(func, new List<Expr>{ x, y}, point, epsilon, tolerance, 10000);
                 Console.WriteLine(result);
             }
             
--- OptimizationMethods/SecondOrderMethods/Markvardt.cs
+++ OptimizationMethods/SecondOrderMethods/Markvardt.cs
@@ -2,30 +2,64 @@ using MathNet.Symbolics;
 using MathNet.Numerics.LinearAlgebra;
 using Expr = MathNet.Symbolics.SymbolicExpression;
 using MathNet.Numerics.Optimization;
+using MathNet.Numerics.LinearAlgebra.Double;
+using static MathNet.Symbolics.VisualExpression;
+using System.Drawing;
 
 namespace OptimizationMethods.SecondOrderMethods;
 
 public class Markvardt : IMethod
 {
-    public static Vector<double> Search(Expr f, Expr x, Expr y, Expr z, Vector<double> initialGuess, double epsilon, double InitTolerance, int maxIterations)
+    static bool IsPositiveDefinite(Matrix<double> matrix)
+    {
+        // Проверяем, что матрица симметрична
+        if (!matrix.IsSymmetric())
+        {
+            return false;
+        }
+
+        // Вычисляем собственные значения
+        var eigenValues = matrix.Evd().EigenValues;
+
+        // Проверяем, что все собственные значения положительны
+        foreach (var eigenValue in eigenValues)
+        {
+            if (eigenValue.Real <= 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+    public static Vector<double> Search(Expr f, List<Expr> vars, Vector<double> initialGuess, double epsilon, double InitTolerance, int maxIterations)
     {
         //градиент функции
-        var gradient = new[] { f.Differentiate(x), f.Differentiate(y), f.Differentiate(z) };
+        Expr[] gradient = new Expr[vars.Count];
+        for (int i = 0; i < vars.Count; i++)
+        {
+            gradient[i]= (f.Differentiate(vars[i]));
+        }
+
         var tolerance = InitTolerance;
         //гессиан функции
-        var hessian = new[,]
+        Expr[,] hessian = new Expr[vars.Count, vars.Count];
+        for(int i =0;i<vars.Count;i++)
         {
-            { f.Differentiate(x).Differentiate(x), f.Differentiate(x).Differentiate(y), f.Differentiate(x).Differentiate(z) },
-            { f.Differentiate(y).Differentiate(x), f.Differentiate(y).Differentiate(y), f.Differentiate(y).Differentiate(z) },
-            { f.Differentiate(z).Differentiate(x), f.Differentiate(z).Differentiate(y), f.Differentiate(z).Differentiate(z) }
-        };
+            for (int j = 0; j < vars.Count; j++)
+            {
+                hessian[i,j] = f.Differentiate(vars[i]).Differentiate(vars[j]);
+            }
+        }
         var currentPoint = initialGuess;
-        var grad = EvaluateGradient(gradient, currentPoint);
-        var hess = EvaluateHessian(hessian, currentPoint);
+        var grad = EvaluateGradient(gradient, currentPoint,vars);
+        var hess = EvaluateHessian(hessian, currentPoint,vars);
+        if (!IsPositiveDefinite(hess))
+            Console.WriteLine("Неудачное начальное приближение, матрица Гесе не положительно определенная");
         int k = 0;
         checkEnd:
         {
-            grad = EvaluateGradient(gradient, currentPoint);
+            grad = EvaluateGradient(gradient, currentPoint,vars);
             if (grad.L2Norm() <= epsilon || k>maxIterations) {
 #if DEBUG
                 Console.WriteLine($"Алгоритм остановился после {k} итераций");
@@ -33,19 +67,18 @@ public class Markvardt : IMethod
                 return currentPoint;
             }
             //goto evalute;
-            hess = EvaluateHessian(hessian, currentPoint);
+            hess = EvaluateHessian(hessian, currentPoint,vars);
         }
         evalute:
         {
             //var hess  = EvaluateHessian(hessian,currentPoint);
             
             //единичная матрица
-            var identityMatrix = Matrix<double>.Build.DenseIdentity(3);
+            var identityMatrix = Matrix<double>.Build.DenseIdentity(vars.Count);
 
             var nextPoint = currentPoint - (hess + tolerance * identityMatrix).Inverse()*grad;
-
-            if (f.Evaluate(new Dictionary<string, FloatingPoint> { { "x", nextPoint[0] }, { "y", nextPoint[1] }, { "z", nextPoint[2] } }).RealValue <
-                f.Evaluate(new Dictionary<string, FloatingPoint> { { "x", currentPoint[0] }, { "y", currentPoint[1] }, { "z", currentPoint[2] } }).RealValue)
+            if (f.Evaluate(BuildPointDict(nextPoint, vars)).RealValue <
+                f.Evaluate(BuildPointDict(currentPoint, vars)).RealValue)
             {
                 currentPoint = nextPoint;
                 k += 1;
@@ -117,37 +150,42 @@ public class Markvardt : IMethod
             }
         }
     }
-    static Vector<double> EvaluateGradient(Expr[] gradient, Vector<double> point)
+    static Vector<double> EvaluateGradient(Expr[] gradient, Vector<double> point, List<Expr> vars)
     {
-        var result = Vector<double>.Build.Dense(3);
-        var symbols = new Dictionary<string, FloatingPoint>
-        {
-            { "x", point[0] },
-            { "y", point[1] },
-            { "z", point[2] }
-        };
+        var result = Vector<double>.Build.Dense(vars.Count);
+        //var symbols = new Dictionary<string, FloatingPoint>
+        //{
+        //    { "x", point[0] },
+        //    { "y", point[1] },
+        //    { "z", point[2] }
+        //};
+        var symbols = BuildPointDict(point, vars);
 
-        for (int i = 0; i < 3; i++)
+        for (int i = 0; i < vars.Count; i++)
             result[i] = gradient[i].Evaluate(symbols).RealValue;
 
         return result;
     }
 
-    static Matrix<double> EvaluateHessian(Expr[,] hessian, Vector<double> point)
+    static Matrix<double> EvaluateHessian(Expr[,] hessian, Vector<double> point, List<Expr> vars)
     {
-        var result = Matrix<double>.Build.Dense(3, 3);
-        var symbols = new Dictionary<string, FloatingPoint>
-        {
-            { "x", point[0] },
-            { "y", point[1] },
-            { "z", point[2] }
-        };
+        var result = Matrix<double>.Build.Dense(vars.Count, vars.Count);
+        var symbols = BuildPointDict(point, vars);
 
-        for (int i = 0; i < 3; i++)
-            for (int j = 0; j < 3; j++)
+        for (int i = 0; i < vars.Count; i++)
+            for (int j = 0; j < vars.Count; j++)
                 result[i, j] = hessian[i, j].Evaluate(symbols).RealValue;
 
         return result;
+    }
+    static Dictionary<string, FloatingPoint> BuildPointDict(Vector<double> point, List<Expr> vars)
+    {
+        var symbols = new Dictionary<string, FloatingPoint>();
+        for (int i = 0; i < vars.Count; i++)
+        {
+            symbols[vars[i].VariableName] = point[i];
+        }
+        return symbols;
     }
 
 }
