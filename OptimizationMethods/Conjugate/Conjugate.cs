2653307e6bbdad226b0fa5f8a93e0af7f85c0ce9
Daniil Pominov <53336146+DaniilPominov@users.noreply.github.com>
2025-03-25 12:28:45 +0700

    Conjugate directions (#4)
    * init
    
    * Набросок метода, метод дробных шагов ведят себя не адекватно по предложенным ему направлениям.
    
    * Дополнительные условия проверки
    
    * Алгоритм удовлетворительно работает с примером из учебника
    
    * Найденые адекватные НУ для демонстрации
    
    * Работа защищена

---
+++ OptimizationMethods/Conjugate/Conjugate.cs
@@ -0,0 +1,73 @@
+using MathNet.Symbolics;
+using MathNet.Numerics.LinearAlgebra;
+using Expr = MathNet.Symbolics.SymbolicExpression;
+using Microsoft.FSharp.Data.UnitSystems.SI.UnitNames;
+using OptimizationMethods.SecondOrderMethods;
+using OptimizationMethods.GradientDescent;
+namespace OptimizationMethods.Conjugate{
+
+//Метод Флетчера-Ривса
+public class Conjugate
+{
+        public static Vector<double> Search(Expr f, List<Expr> vars,
+            Vector<double> initialPoint, double epsilon, int M,
+            double secondEpsilon = 1.0 / 100.0, double stepSplitInit = 0.1, 
+            double stepSplitCoeff = 0.4
+        )
+    {
+        int k = 0;
+        Expr[] gradient = new Expr[vars.Count];
+        Vector<double> grad = null;
+        Vector<double> p = null;
+        double beta = 1;
+
+        var currentPoint = initialPoint.Clone();
+        var oldPoint = initialPoint.Clone();
+
+        for (int i = 0; i < vars.Count; i++)
+        {
+            gradient[i]= f.Differentiate(vars[i]);
+        }
+        second:{
+            grad = Markvardt.EvaluateGradient(gradient,currentPoint,vars);
+                bool fDifference = false;
+                if (k > 1)
+                {
+                    fDifference = f.Evaluate(Common.BuildPointDict(currentPoint, vars)).RealValue -
+                    f.Evaluate(Common.BuildPointDict(oldPoint, vars)).RealValue < epsilon;
+                }
+            if(grad.L2Norm()<=epsilon || k>M || fDifference)
+                {
+                    Console.WriteLine($"iterations left:{k}");
+                        return currentPoint;
+            }
+
+        }
+        thirth:{
+
+                if (k == 0 || p == null)
+                    p = Vector<double>.Build.Dense((-grad).ToArray());
+                else
+                {
+                    var oldGrad = Markvardt.EvaluateGradient(gradient, oldPoint, vars);
+
+                    beta = (grad.L2Norm()* grad.L2Norm()) / (oldGrad.L2Norm()* oldGrad.L2Norm());
+                    p = -grad + p * beta;
+                }
+            var Lsub = StepSplitting.Search(f,vars,currentPoint,
+                secondEpsilon, stepSplitInit, stepSplitCoeff,
+                -p);
+            var L = (Lsub - currentPoint) *(p) /(p.L2Norm()* p.L2Norm());
+            var buff = currentPoint.Clone();
+            currentPoint = currentPoint + L*p;
+            oldPoint = buff.Clone();
+            k++;
+            if(k>=M){
+                return currentPoint;
+            }
+            goto second;
+        }
+        
+    }
+}
+}
--- OptimizationMethods/Program.cs
+++ OptimizationMethods/Program.cs
@@ -1,7 +1,6 @@
 using MathNet.Numerics.LinearAlgebra;
-using OptimizationMethods.DirectedMethods;
-using OptimizationMethods.SecondOrderMethods;
-using System.Collections.Generic;
+using OptimizationMethods.Conjugate;
+using OptimizationMethods.GradientDescent;
 using Expr = MathNet.Symbolics.SymbolicExpression;
 
 namespace OptimizationMethods
@@ -11,22 +10,21 @@ namespace OptimizationMethods
         static void Main(string[] args)
         {
 
-            double epsilon = Math.Pow(10, -5);
+            double epsilon = Math.Pow(10, -8);
             double tolerance = 20;
 
             var x = Expr.Variable("x");
             var y = Expr.Variable("y");
             var z = Expr.Variable("z");
-            var x1 = -0.5;
-            var x2 = -0.5;
-            var x3 = 1;
-            //var x3 = 0.0;
+            var x1 = 0.82;
+            var x2 = 0.79;
+            var x3 = 1.3;
             var testPoints = new[] { 
                 Vector<double>.Build.Dense([x1, x2,x3]),
             };
             var vars = new List<Expr> { x, y,z};
-            Expr func = x.Pow(5) -5*x + +10*(-x.Pow(2)+y).Pow(2)+z.Pow(3)-1-6*z;
-            
+            Expr func = x.Pow(2) -5*x + +10*(-x.Pow(2)+y).Pow(2)+z.Pow(2)-1-6*z;
+            //Expr func = 2 * x.Pow(2) + x * y + y.Pow(2);
             //Expr func = 2*x.Pow(4)*y.Pow(4)+x.Pow(2)*y.Pow(2)+z.Pow(4)+x.Pow(2)*z.Pow(2)+x+y;
             //for initial (-1/2,-1/2,0) Critical point found at (x, y, z) = (-0.658417, -0.658417, 0.000000)
             //Expr func = x.Pow(2) + 5*(30*y-x).Pow(4);
@@ -39,10 +37,10 @@ namespace OptimizationMethods
             {
                 Console.WriteLine(point);
                 //var result = Markvardt.Search(func,vars,point,epsilon,20000,10000);
-                var result = Configuration.Search(func, vars, point,epsilon,1,2, new List<double> { 1,1,1});
-                Console.WriteLine($"point={result}, f(M)={func.Evaluate(Common.BuildPointDict(result,vars)).RealValue}");
+                var result = Conjugate.Conjugate.Search(func, vars, point, epsilon, 200,epsilon,0.01,0.1);
+                Console.WriteLine($"point={result}, f(M)={func.Evaluate(Common.BuildPointDict(result, vars)).RealValue}");
             }
-            
+
         }
     }
 }
