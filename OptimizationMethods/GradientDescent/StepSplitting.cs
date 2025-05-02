512286aa0bce0bcbeb5149824baca5627fd94869
Daniil Pominov <d.pominov2016@yandex.ru>
2025-05-02 21:11:26 +0700

    prototype

--- OptimizationMethods/GradientDescent/StepSplitting.cs
+++ OptimizationMethods/GradientDescent/StepSplitting.cs
@@ -1,5 +1,4 @@
-﻿using MathNet.Symbolics;
-using Expr = MathNet.Symbolics.SymbolicExpression;
+﻿using Expr = MathNet.Symbolics.SymbolicExpression;
 using MathNet.Numerics.LinearAlgebra;
 using OptimizationMethods.SecondOrderMethods;
 namespace OptimizationMethods.GradientDescent
@@ -68,6 +67,105 @@ namespace OptimizationMethods.GradientDescent
 #endif
             return currentPoint;
 
+        }
+
+        private static Expr BuildPenalizedFunction(Expr f, List<Expr> equalityConstraints, List<Expr> inequalityConstraints, double penaltyCoefficient, double epsilon = 1e-8)
+        {
+            Expr penalized = f;
+
+            if (equalityConstraints != null)
+            {
+                foreach (var h in equalityConstraints)
+                {
+                    //penalized += penaltyCoefficient * h.Pow(2);
+                    penalized += penaltyCoefficient * h.Pow(2);
+                }
+            }
+
+            if (inequalityConstraints != null)
+            {
+                foreach (var g in inequalityConstraints)
+                {
+                    var smoothMax = (g + (g.Pow(2) + epsilon).Sqrt()) / 2;
+                    penalized += penaltyCoefficient * smoothMax.Pow(2);
+                }
+            }
+
+            return penalized;
+        }
+
+        internal static Vector<double> Search(
+            Expr f,
+            List<Expr> vars,
+            Vector<double> initialGuess,
+            double epsilon,
+            double initStep,
+            double? d,
+            Vector<double> otherMethodDirection = null,
+            List<Expr> equalityConstraints = null,
+            List<Expr> inequalityConstraints = null,
+            double penaltyCoefficient = 1.0)
+        {
+            int maxIterations = 100000;
+            var stepSize = initStep;
+            var delta = d ?? 0.5;
+#if DEBUG
+            int iterCount = 0;
+#endif
+            Expr functionToUse = f;
+            if ((equalityConstraints?.Count ?? 0) > 0 || (inequalityConstraints?.Count ?? 0) > 0)
+            {
+                functionToUse = BuildPenalizedFunction(f, equalityConstraints, inequalityConstraints, penaltyCoefficient);
+            }
+
+            Expr[] gradient = new Expr[vars.Count];
+            for (int i = 0; i < vars.Count; i++)
+            {
+                gradient[i] = functionToUse.Differentiate(vars[i]);
+            }
+
+            var currentPoint = initialGuess.Clone();
+
+            for (int i = 0; i < maxIterations; i++)
+            {
+                var grad = Markvardt.EvaluateGradient(gradient, currentPoint, vars);
+                if (otherMethodDirection != null)
+                    grad = otherMethodDirection;
+
+                var nextPoint = currentPoint - stepSize * grad;
+
+                if (grad.L2Norm() < epsilon)
+                {
+#if DEBUG
+                    iterCount = i;
+#endif
+                    break;
+                }
+
+                double currentValue = functionToUse.Evaluate(Common.BuildPointDict(currentPoint, vars)).RealValue;
+                double nextValue = functionToUse.Evaluate(Common.BuildPointDict(nextPoint, vars)).RealValue;
+
+                while (nextValue > currentValue)
+                {
+                    stepSize *= delta;
+                    nextPoint = currentPoint - stepSize * grad;
+
+                    if (stepSize <= epsilon)
+                    {
+                        return nextPoint;
+                    }
+
+                    nextValue = functionToUse.Evaluate(Common.BuildPointDict(nextPoint, vars)).RealValue;
+                }
+
+                stepSize = initStep;
+                currentPoint = nextPoint;
+            }
+
+#if DEBUG
+            Console.WriteLine($"Конечный размер шага= {stepSize} при начальном= {initStep}, прошло итераций= {iterCount}");
+#endif
+            return currentPoint;
         }
     }
 }
--- OptimizationMethods/Program.cs
+++ OptimizationMethods/Program.cs
@@ -10,36 +10,28 @@ namespace OptimizationMethods
         static void Main(string[] args)
         {
 
-            double epsilon = Math.Pow(10, -8);
-            double tolerance = 20;
+            var vars = new List<Expr> { Expr.Variable("x"), Expr.Variable("y") };
+            Expr objective = Expr.Parse("x+y");
 
-            var x = Expr.Variable("x");
-            var y = Expr.Variable("y");
-            var z = Expr.Variable("z");
-            var x1 = 0.82;
-            var x2 = 0.79;
-            var x3 = 1.3;
-            var testPoints = new[] { 
-                Vector<double>.Build.Dense([x1, x2,x3]),
-            };
-            var vars = new List<Expr> { x, y,z};
-            Expr func = x.Pow(2) -5*x + +10*(-x.Pow(2)+y).Pow(2)+z.Pow(2)-1-6*z;
-            //Expr func = 2 * x.Pow(2) + x * y + y.Pow(2);
-            //Expr func = 2*x.Pow(4)*y.Pow(4)+x.Pow(2)*y.Pow(2)+z.Pow(4)+x.Pow(2)*z.Pow(2)+x+y;
-            //for initial (-1/2,-1/2,0) Critical point found at (x, y, z) = (-0.658417, -0.658417, 0.000000)
-            //Expr func = x.Pow(2) + 5*(30*y-x).Pow(4);
-            //Expr func = 2*x.Pow(2)+x*y+y.Pow(2);
-            //Example from Panteleev's book, works with (0.5,1), E = 0.1, tol = 20;
-            
-            //Expr func = 4 * (x - 5).Pow(2) + (y - 6).Pow(2);
-            Console.WriteLine(func);
-            foreach (var point in testPoints)
-            {
-                Console.WriteLine(point);
-                //var result = Markvardt.Search(func,vars,point,epsilon,20000,10000);
-                var result = Conjugate.Conjugate.Search(func, vars, point, epsilon, 200,epsilon,0.01,0.1);
-                Console.WriteLine($"point={result}, f(M)={func.Evaluate(Common.BuildPointDict(result, vars)).RealValue}");
-            }
+            // Ограничения: x + y = 1, y >= 0
+            //var equality = new List<Expr> { Expr.Parse("x + y - 1") };
+            //var inequality = new List<Expr> { Expr.Parse("-y") };
+            var inequality = new List<Expr> { 
+                Expr.Parse("x^2+2*x+y^2-2*y-14"),
+                Expr.Parse("2*x+y"),
+                Expr.Parse("1-x-2*y")};
+            var result = StepSplitting.Search(
+                f: objective,
+                vars: vars,
+                initialGuess: Vector<double>.Build.Dense(new[] { 2.0, 3.0 }),
+                epsilon: 1e-6,
+                initStep: 0.1,
+                d: 0.5,
+                //equalityConstraints: equality,
+                inequalityConstraints: inequality,
+                penaltyCoefficient: 10.0
+            );
+            Console.WriteLine(result);
 
         }
     }
