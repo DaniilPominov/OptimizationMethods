e4219292a43b19f225bb9bc12e3fde64ba6f7921
Daniil Pominov <d.pominov2016@yandex.ru>
2025-05-10 19:17:33 +0700

    рабочий прототип

--- OptimizationMethods/Program.cs
+++ OptimizationMethods/Program.cs
@@ -1,6 +1,7 @@
 using MathNet.Numerics.LinearAlgebra;
 using OptimizationMethods.Conjugate;
 using OptimizationMethods.GradientDescent;
+using OptimizationMethods.RandomSearch;
 using Expr = MathNet.Symbolics.SymbolicExpression;
 
 namespace OptimizationMethods
@@ -11,35 +12,27 @@ namespace OptimizationMethods
         {
 
             double epsilon = Math.Pow(10, -8);
-            double tolerance = 20;
 
             var x = Expr.Variable("x");
             var y = Expr.Variable("y");
             var z = Expr.Variable("z");
-            var x1 = 0.82;
-            var x2 = 0.79;
-            var x3 = 1.3;
-            var testPoints = new[] { 
-                Vector<double>.Build.Dense([x1, x2,x3]),
+
+            var x1 = 1.0;
+            var x2 = 2.0;
+            //var x3 = 3.0;
+            var initial = Vector<double>.Build.Dense([x1,
+                x2, 
+                //x3
+                ]);
+
+            var vars = new List<Expr> { x, 
+                y,
+                //z
             };
-            var vars = new List<Expr> { x, y,z};
-            Expr func = x.Pow(2) -5*x + +10*(-x.Pow(2)+y).Pow(2)+z.Pow(2)-1-6*z;
-            //Expr func = 2 * x.Pow(2) + x * y + y.Pow(2);
-            //Expr func = 2*x.Pow(4)*y.Pow(4)+x.Pow(2)*y.Pow(2)+z.Pow(4)+x.Pow(2)*z.Pow(2)+x+y;
-            //for initial (-1/2,-1/2,0) Critical point found at (x, y, z) = (-0.658417, -0.658417, 0.000000)
-            //Expr func = x.Pow(2) + 5*(30*y-x).Pow(4);
-            //Expr func = 2*x.Pow(2)+x*y+y.Pow(2);
-            //Example from Panteleev's book, works with (0.5,1), E = 0.1, tol = 20;
-            
-            //Expr func = 4 * (x - 5).Pow(2) + (y - 6).Pow(2);
+            Expr func = x.Pow(2) + y.Pow(2);
             Console.WriteLine(func);
-            foreach (var point in testPoints)
-            {
-                Console.WriteLine(point);
-                //var result = Markvardt.Search(func,vars,point,epsilon,20000,10000);
-                var result = Conjugate.Conjugate.Search(func, vars, point, epsilon, 200,epsilon,0.01,0.1);
-                Console.WriteLine($"point={result}, f(M)={func.Evaluate(Common.BuildPointDict(result, vars)).RealValue}");
-            }
+            var result = RandomWithReturn.Search(func, vars, initial, 0.1, 1000, 0.5, 1e-5, 300);
+            Console.WriteLine(result);
 
         }
     }
---
+++ OptimizationMethods/Random/RandomWithReturn.cs
@@ -0,0 +1,66 @@
+﻿using MathNet.Symbolics;
+using MathNet.Numerics.LinearAlgebra;
+using Expr = MathNet.Symbolics.SymbolicExpression;
+
+namespace OptimizationMethods.RandomSearch
+{
+    public class RandomWithReturn
+    {
+
+        public static Vector<double> Search(Expr f, List<Expr> vars, Vector<double> initialGuess,
+            double initialStep, int maxIterations, double beta, double minStepSize,
+            int maxFailedAttempts) 
+        {
+            int currentIteration = 0;
+            double step = initialStep;
+            var currentPoint = initialGuess.Clone();
+            int currentAttempt = 1;
+            while (currentIteration++ < maxIterations)
+            {
+                Vector<double> direction = GenerateRandomDirection(vars.Count);
+                Vector<double> candidatePoint = currentPoint + step * direction;
+
+
+                double currentValue = f.Evaluate(Common.BuildPointDict(currentPoint,vars)).RealValue;
+                double candidateValue = f.Evaluate(Common.BuildPointDict(candidatePoint, vars)).RealValue;
+
+                if (candidateValue < currentValue)
+                {
+                    // Успешный шаг
+                    currentPoint = candidatePoint;
+                    currentIteration++;
+                    currentAttempt = 1;
+                }
+                else
+                {
+                    // Неудачный шаг
+                    currentAttempt++;
+                    if (currentAttempt > maxFailedAttempts)
+                    {
+                        step *= beta;
+                        currentAttempt = 1;
+
+                        if (step <= minStepSize)
+                            break;
+                    }
+                }
+            }
+
+            return currentPoint;
+        }
+
+        // Генерация случайного направления
+        private static Vector<double> GenerateRandomDirection(int dimensions)
+        {
+            double[] components = new double[dimensions];
+            for (int i = 0; i < dimensions; i++)
+            {
+                var random = new Random(DateTime.Now.Microsecond);
+                components[i] = random.NextDouble() * 2 - 1;
+            }
+            Vector<double> vector = Vector<double>.Build.DenseOfArray(components);
+            return vector.Normalize(2);
+        }
+
+    }
+}
